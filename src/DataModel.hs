17e82d8a73c8dc6c471f2dabf89f6467b7bf6a29
Harley D. Eades III <harley.eades@gmail.com>
2024-03-14 10:14:43 -0400

    Decided to roll my own parser.
    The language is simple enough, and I'll get better
    error messages.

--- src/DataModel.hs
+++ src/DataModel.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+{-# OPTIONS_GHC -Wno-unused-matches #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module DataModel where
     import GHC.Generics
-
+    
     data Time = Time {
         hours :: Int,
         minutes :: Int,
@@ -46,6 +49,62 @@ module DataModel where
         measures :: [Measure],
         submovements :: [Movement]
     } deriving (Generic, Show)
+
+    data MovementParams =
+          DescriptionParam String
+        | NotesParam String
+        | LabelsParam [String]
+        | TargetsParam [String]
+        | IterationParam Iteration
+        | ScalersParam [Scalers]
+        | MeasuresParam [Measure]
+        | SubmovementsParam [[MovementParams]]
+      deriving (Generic, Show)       
+
+    fromMovementParams :: [MovementParams] -> Maybe Movement
+    fromMovementParams ps = do
+        description <- findDescriptionParam ps
+        notes <- findNotesParam ps
+        labels <- findLabelsParam ps
+        targets <- findTargetsParam ps
+        iteration <- findIterationParam ps
+        scalers <- findScalersParam ps
+        measures <- findMeasuresParam ps
+        submovementsParams <- findSubmovementsParam ps        
+        submovements <- mapM fromMovementParams submovementsParams
+        return (Movement description notes labels targets iteration scalers measures submovements)
+        where
+            findDescriptionParam [] = Nothing
+            findDescriptionParam (DescriptionParam d:_) = Just d
+            findDescriptionParam (_:ps) = findDescriptionParam ps
+
+            findNotesParam [] = Nothing
+            findNotesParam (NotesParam s:_) = Just s
+            findNotesParam (_:ps) = findNotesParam ps
+
+            findLabelsParam [] = Nothing
+            findLabelsParam (LabelsParam l:_) = Just l
+            findLabelsParam (_:ps) = findLabelsParam ps
+
+            findTargetsParam [] = Nothing
+            findTargetsParam (TargetsParam t:_) = Just t
+            findTargetsParam (_:ps) = findTargetsParam ps
+
+            findIterationParam [] = Nothing
+            findIterationParam (IterationParam i:_) = Just i
+            findIterationParam (_:ps) = findIterationParam ps
+
+            findScalersParam [] = Nothing
+            findScalersParam (ScalersParam s:_) = Just s
+            findScalersParam (_:ps) = findScalersParam ps
+
+            findMeasuresParam [] = Nothing
+            findMeasuresParam (MeasuresParam m:_) = Just m            
+            findMeasuresParam (_:ps) = findMeasuresParam ps
+
+            findSubmovementsParam [] = Nothing
+            findSubmovementsParam (SubmovementsParam m:_) = Just m
+            findSubmovementsParam (_:ps) = findSubmovementsParam ps
 
     data BlockIteration =
           Amrap String
